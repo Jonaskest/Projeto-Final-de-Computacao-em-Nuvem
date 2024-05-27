aeb711a5d5cf8c2e3f4fac971b8ad7e0d8557a0e
Jonas <jonaskest@hotmail.com>
2024-05-27 16:13:28 -0300

    update main

--- Projeto_django/settings.py
+++ Projeto_django/settings.py
@@ -16,7 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Projeto_django', 'static')]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
